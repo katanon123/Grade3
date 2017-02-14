cf47e159ea1e4ca4caad385f589ac133c4cb9e9d
katanon123 <katakarn121@gmail.com>
2017-02-14 11:53:53 +0700

    Updare Code

--- src/grade3/IGread.java
+++ src/grade3/IGread.java
@@ -3,18 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package th.ac.npru..se.grade3;
 
-/**
- *
- * @author katanon2
- */
-interface IGread {
-  
-    public String Test_score();
-  
-    
+
+package th.ac.npru.se.grade;
+
+public interface Computable {
+	String computeGrade(float point);
 }
-
-    
 
