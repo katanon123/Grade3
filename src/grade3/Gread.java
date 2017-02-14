fd638d5120af34f6d8b350dda59d3cd398eb2508
katanon123 <katakarn121@gmail.com>
2017-02-14 11:55:01 +0700

    Update code
    Java

--- src/grade3/Gread.java
+++ src/grade3/Gread.java
@@ -3,46 +3,41 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package  th.ac.npru..se.grade3;
+package th.ac.npru.se.grade;
 
+public class EnglishSubject implements Computable {
 
+	String grade;
+	final String GRADE_A = "A";
+	final String GRADE_B_PLUS = "B+";
+	final String GRADE_B = "B";
+	final String GRADE_C_PLUS = "C+";
+	final String GRADE_C = "C";
+	final String GRADE_D_PLUS = "D+";
+	final String GRADE_D = "D";
+	final String GRADE_E = "E";
+	@Override
+	public String computeGrade(float point) {
+		// TODO Auto-generated method stub
+		if(point >= 80){
+			grade = GRADE_A; 
+		}else if(point >=75){
+			grade = GRADE_B_PLUS;
+		}else if(point >= 70){
+			grade = GRADE_B;
+		}else if(point >= 65){
+			grade = GRADE_C_PLUS;
+		}else if(point >= 60){
+			grade = GRADE_C;
+		}else if(point >= 55){
+			grade = GRADE_D_PLUS;
+		}else if(point >= 50){
+			grade = GRADE_D;
+		}else{
+			grade = GRADE_E;
+		}
+		
+		return grade;
+	}
 
-
-
-/**
- *
- * @author katanon2
- */
-public class Gread implements IGread{
-           String Grade;
-            int Test_score ;
-        public String Test_score() {      
-        if (Test_score >= 80) {
-            Grade = "A";
-        } else if (Test_score >= 79) {
-            Grade = "B+";
-             } else if (Test_score >= 74) {
-            Grade = "B";
-        } else if (Test_score >= 70) {
-            Grade = "C+";
-             } else if (Test_score >= 64) {
-            Grade = "C";
-        } else if (Test_score >= 60) {
-            Grade = "D+";
-        } else if (Tests_core >= 54) {
-            Grade = "D";
-        } else {
-            Grade = "I";
-        }
-
-    return Grade;    
 }
-}
-
-
-   
-    
-
-  
-    
-
